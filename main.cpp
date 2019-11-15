d8a05ad73f5587fa222b644a021d0fc570e87421
Saqib Ali <business.saqib@gmail.com>
2019-11-15 10:39:09 +0500

    Board printing functionality added.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <conio.h>
 
 using namespace std;
 
+#define CELL_WIDTH 4
+
+void printBoard(int board[][8]) {
+
+    for (int k = 0; k < 66; k++)
+        (k == 65)? cout << endl : cout << "-";
+
+    for (int i = 0; i < 8; i++) {
+        cout << "|";
+        for (int j = 0; j < 8; j++) {
+            cout << setw(CELL_WIDTH) << board[i][j];
+            cout << setw(CELL_WIDTH) << "|";
+        }
+        cout << endl;
+        for (int k = 0; k < 67; k++)
+            (k == 66)? cout << endl : cout << "-";
+    }
+
+}
+
 int main() {
 
-    cout << "Welcome to CPP-CHESS" << endl;
+    int board[8][8] = {
+            {-1,-2,-3,-4,-4,-3,-2,-1},
+            {-6,-6,-6,-6,-6,-6,-6,-6},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {6, 6, 6, 6, 6, 6, 6, 6},
+            {1, 2, 3, 4, 4, 3, 2, 1}
+    };
 
+    printBoard(board);
+
+    getch();
     return 0;
 }
